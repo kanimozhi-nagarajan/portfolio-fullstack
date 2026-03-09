099cbd45f992ba03b81118baa8230e9a579b1ad6
kanimozhi-nagarajan <kanimozhin166@gmail.com>
2026-03-10 03:32:36 +0530

    Feat:Initialized Tailwind CSS

--- portfolio-client/src/App.jsx
+++ portfolio-client/src/App.jsx
@@ -3,6 +3,11 @@ function App() {
     <div>
       <h1>My Portfolio</h1>
       <p>Welcome to my developer journey.</p>
+      <div className="h-screen flex items-center justify-center bg-slate-900">
+        <h1 className="text-4xl font-bold text-white">
+          Tailwind CSS v4 is working 🚀
+        </h1>
+      </div>
     </div>
   );
 }

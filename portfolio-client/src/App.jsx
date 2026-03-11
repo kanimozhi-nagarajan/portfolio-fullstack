ecf1e0ee9da263a071f96976c5566a167082524c
kanimozhi-nagarajan <kanimozhin166@gmail.com>
2026-03-11 21:38:19 +0530

    Feat: Included Navbar Component

--- portfolio-client/src/App.jsx
+++ portfolio-client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
@@ -10,6 +11,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ portfolio-client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-slate-900 text-white shadow-md">
+      <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
+        <h1 className="text-xl font-bold">Portfolio</h1>
+        <div className="space-x-6">
+          <Link className="hover:text-sky-400 transition" to="/">
+            Home
+          </Link>
+          <Link className="hover:text-sky-400 transition" to="/about">
+            {" "}
+            About
+          </Link>
+          <Link className="hover:text-sky-400 transition" to="/projects">
+            {" "}
+            Projects
+          </Link>
+          <Link className="hover:text-sky-400 transition" to="/skills">
+            {" "}
+            Skills
+          </Link>
+          <Link className="hover:text-sky-400 transition" to="/journey">
+            {" "}
+            Journey
+          </Link>
+          <Link className="hover:text-sky-400 transition" to="/contact">
+            {" "}
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

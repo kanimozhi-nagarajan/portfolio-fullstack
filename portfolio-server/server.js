06e94a36f08ee96ec9a126330b784680b7aa14dc
kanimozhi-nagarajan <kanimozhin166@gmail.com>
2026-03-17 22:58:17 +0530

    Refactor: removed uploads folder , update .env , updated cors in server

--- portfolio-server/server.js
+++ portfolio-server/server.js
@@ -13,7 +13,13 @@ connectMongo();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));

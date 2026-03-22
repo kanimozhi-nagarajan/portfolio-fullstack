eb13447d572f3c611452629e9ffa8b35ef6de912
kanimozhi-nagarajan <kanimozhin166@gmail.com>
2026-03-22 08:12:57 +0530

    Chore: Loader issue fixed

--- portfolio-client/src/components/Loader.jsx
+++ portfolio-client/src/components/Loader.jsx
@@ -2,8 +2,10 @@ function Loader() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-[#0f172a]   z-50">
       <div className="w-12 h-12 border-4 border-sky-400 border-t-transparent rounded-full animate-spin">
-        Since this site use vercel for free hosting , longtime in activity might
-        go to sleep , kindly refresh or revist to see the site
+        <p>
+          Since this site use vercel for free hosting , longtime in activity
+          might make sight unresponsive , please be patient ! Thank you
+        </p>
       </div>
     </div>
   );

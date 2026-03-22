c828298dc6c709a6ef3af3f0672bffc446632aa2
kanimozhi-nagarajan <kanimozhin166@gmail.com>
2026-03-22 08:42:37 +0530

    Loader testing 55

--- portfolio-client/src/components/Loader.jsx
+++ portfolio-client/src/components/Loader.jsx
@@ -2,23 +2,16 @@ function Loader() {
   return (
     // <div className="fixed inset-0 flex items-center justify-center bg-[#0f172a]   z-50">
     //    <div className="w-12 h-12 border-4 border-sky-400 border-t-transparent rounded-full animate-spin">
-    //   <p>
-    //     Since this site use vercel for free hosting , longtime in activity might
-    //     make sight unresponsive , please be patient ! Thank you
-    //   </p>
     //    </div>
     //    </div>
-    <div className="text-4xl font-bold text-center mb-12 text-green-400 font-mono">
-      <p>Site may sleep on free hosting — if it’s slow, just refresh 🔄</p>
+    <div>
+      <h2 className="text-4xl font-bold text-center mb-12 text-green-400 font-mono">
+        &gt;{" "}
+        <TypingText text=" Site may sleep on free hosting — if it’s slow, just refresh 🔄" />
+      </h2>
     </div>
-    // <div>
-    //   <h2 className="text-4xl font-bold text-center mb-12 text-green-400 font-mono">
-    //     &gt;{" "}
-    //     <TypingText
-    //       text=" Since this site use vercel for free hosting , longtime in activity might
-    //     make sight unresponsive , please be patient ! Thank you"
-    //     />
-    //   </h2>
+    // <div className="text-4xl font-bold text-center mb-12 text-green-400 font-mono">
+    //   <p>Site may sleep on free hosting — if it’s slow, just refresh 🔄</p>
     // </div>
   );
 }
